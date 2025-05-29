190a6060e2ef1bece357f9b7d6c64467ad6c4cf2
gaurav662004 <gbwaghmare66@gmail.com>
2025-05-29 14:31:55 +0530

    Removes blog5 content and adds Suspense to Gallery
    Removes the static content of blog5 page and replaces it with a minimal component.
    
    Adds React Suspense to the Gallery component to handle image loading and improve user experience by displaying a loading indicator.

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,9 +1,12 @@
 import Picture from "../../components/gallery/Picture";
+import { Suspense } from "react";
 
 const Gallery = () => {
-    return ( 
-        <Picture/>
-     );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Picture/>
+        </Suspense>
+    );
 }
  
 export default Gallery;
