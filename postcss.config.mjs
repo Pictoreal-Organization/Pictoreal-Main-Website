a5a4b33866c7572131240940b5d75a34d3b8db9a
riddhilahare14 <lahare.riddhi14@gmail.com>
2025-08-23 17:25:22 +0530

    upgrade TailwindCSS to v4 and update PostCSS config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,15 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// };
+
+// export default config;
+
+// postcss.config.mjs
+export default {
+  plugins: ['@tailwindcss/postcss'],
 };
 
-export default config;
