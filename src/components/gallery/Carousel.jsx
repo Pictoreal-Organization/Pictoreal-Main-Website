14e51e6e7fae419458f16359712c4cbab81c2cf5
Tushar <tushartalekar2024@gmail.com>
2025-09-09 19:34:46 +0530

    Gallery page :Change color, margin, hover effects and other suggestions

--- src/components/gallery/Carousel.jsx
+++ src/components/gallery/Carousel.jsx
@@ -31,49 +31,52 @@ const Carousel = ({ images, onImageClick }) => {
   };
 
   return (
+    // <div className="p-10">
+
     <div
-      className="relative lg:max-w-full max-w-[85%] mx-auto overflow-hidden my-2 lg:my-8 z-20"
+      className="relative lg:max-w-full max-w-[85%] mx-auto  my-2 lg:my-8 z-5"
       ref={containerRef}
-    >
-      <div className="relative w-full h-auto flex items-center justify-center bg-white-200 rounded-lg">
+      >
+      <div className="relative w-full h-auto flex overflow-hidden items-center py-10 justify-center bg-white-200 rounded-lg">
         <div
-          className="flex transition-transform duration-1000 ease-in-out"
+          className="flex transition-transform  duration-1000 ease-in-out"
           style={{
             transform: `translateX(-${currentIndex * 100}%)`,
             width: `${images.length * 100}%`,
           }}
-        >
+          >
           {images.map((image, index) => (
             <div
-              key={index}
-              className="flex-shrink-0 h-80 flex items-center justify-center w-full"
+            key={index}
+            className="flex-shrink-0 h-80 flex items-center  justify-center w-full"
             >
               <img
                 ref={index === 0 ? imageRef : null}
                 src={image}
                 alt={`Slide ${index}`}
-                className="max-h-full object-cont  border-2 border-firefly cursor-pointer rounded-lg transition-transform duration-500 hover:scale-105 hover:shadow-xl"
+                className="max-h-full object-cont border-2 cursor-pointer rounded-lg transition-transform duration-500 hover:scale-105 hover:shadow-xl"
                 onClick={() => onImageClick(images)}
-              />
+                />
             </div>
           ))}
         </div>
 
+      </div>
         <button
           onClick={prevSlide}
-          className=" md:block absolute top-1/2  left-0 md:left-0 transform -translate-y-1/2 bg-firefly text-white bg-deepnavy p-3 rounded-full hover:bg-[#00426b] transition-colors duration-300 z-10"
-        >
+          className=" md:block absolute top-1/2  -left-9 md:left-0 transform -translate-y-1/2 bg-firefly text-white bg-deepnavy p-3 rounded-full hover:bg-[#00426b] transition-colors duration-300 z-10"
+          >
           &#10094;
         </button>
 
         <button
           onClick={nextSlide}
-          className=" md:block absolute top-1/2 right-0 md:right-0 transform -translate-y-1/2 bg-firefly text-white bg-deepnavy p-3 rounded-full hover:bg-[#00426b] transition-colors duration-300"
-        >
+          className=" md:block absolute top-1/2 -right-9 md:right-0 transform -translate-y-1/2 bg-firefly text-white bg-deepnavy p-3 rounded-full hover:bg-[#00426b] transition-colors duration-300 z-10"
+          >
           &#10095;
         </button>
-      </div>
     </div>
+          // </div>
   );
 };
 
--- src/components/gallery/Modal.jsx
+++ src/components/gallery/Modal.jsx
@@ -5,21 +5,25 @@ const Modal = ({ isOpen, onClose, images, onImageClick }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 !m-auto  bg-[#b9dbf2] bg-opacity-90 flex items-center justify-center z-50 p-4 transition-opacity duration-500 ease-in-out">
-      <div className="relative bg-[#dddddd] p-6 rounded-lg max-w-5xl w-full max-h-screen overflow-y-auto shadow-lg transition-transform duration-300 transform scale-95 hover:scale-100">
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-[#141414] z-50 bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition-colors duration-300"
-        >
-          &#10005;
-        </button>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="fixed inset-0 !m-auto  bg-[#A8DCEC] bg-opacity-90 flex items-center justify-center z-50 p-4 transition-opacity duration-500 ease-in-out">
+      <div className="relative bg-[#DCF1FF] p-6 rounded-lg max-w-5xl w-full max-h-screen overflow-y-auto shadow-lg transition-transform duration-300 transform scale-95 ">
+        <div className="absolut sticky top-0 right-0 flex justify-end rounded-full">
+          <div className="bg-red-600 flex items-center justify-center h-10 w-10 p-2 absolute top-0 right-0 rounded-full hover:bg-red-700 ">
+            <button
+              onClick={onClose}
+              className=" text-white z-50 m-auto transition-colors duration-300"
+            >
+              &#10005;
+            </button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-10 lg:grid-cols-4 gap-4">
           {images.map((image, index) => (
             <img
               key={index}
               src={image}
               alt={`Image ${index}`}
-              className="w-full h-64 object-cover border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+              className="w-full h-64 object-cover border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-102 hover:shadow-lg"
               onClick={() => onImageClick(image)}
             />
           ))}
